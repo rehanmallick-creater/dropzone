71f55d03ef30a0ec533d7a4375c93323b45a9d4f
rehanmallick-creater <rehanalaamm@gmail.com>
2026-03-24 03:50:41 +0530

    update db.js from repository

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,15 @@
-const mongoose=require('mongoose');
-const dotenv =require('dotenv');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 dotenv.config();
-console.log('MONGO_URI:', process.env.MONGO_URI);
-const connectDB=async()=>{
-    try{
-        const conn=await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected:`);
 
-    }catch(error){
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
-
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
-module.exports =connectDB;
+
+module.exports = connectDB;
