9c8fbb7c94f9de6e3a0d8568148f0f3f2a55a914
rehanmallick-creater <rehanalaamm@gmail.com>
2026-03-24 02:35:45 +0530

    Fix CORS allow all origins

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,9 @@ connectDB();
 
 const app=express();
 app.use(cors({
-  origin: [
-    'http://127.0.0.1:5500',
-    'http://localhost:5500',
-    'https://dropzone-brown.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
